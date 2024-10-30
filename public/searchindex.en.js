cfcc58b7c107d2d2dc55463c439e0f9e72514652
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2024-10-30 20:57:19 +0100

    Begin MicroWin docs

--- public/searchindex.en.js
+++ public/searchindex.en.js
@@ -1,6 +1,30 @@
 var relearn_searchindex = [
   {
-    "breadcrumb": "Home Page",
+    "breadcrumb": "Welcome to the WinUtil Documentation! \u003e User-Guides",
+    "content": "MicroWin is our in-house solution for customized and debloated Windows images. With MicroWin, you can create images that have minimal bloat and no interruptions. This has an upside: you can get more done and use Windows the way you want.\nDue to MicroWin using standard Windows system administration tools, such as DISM (Deployment Image Servicing and Management), advanced Windows users (system administrators and tech enthusiasts) can continue making changes so that they can have their own set of customizations with their ISO file.\nIf you’re excited to try this out, let’s go through how to use it. You’ll be done in a short time",
+    "description": "MicroWin is our in-house solution for customized and debloated Windows images. With MicroWin, you can create images that have minimal bloat and no interruptions. This has an upside: you can get more done and use Windows the way you want.\nDue to MicroWin using standard Windows system administration tools, such as DISM (Deployment Image Servicing and Management), advanced Windows users (system administrators and tech enthusiasts) can continue making changes so that they can have their own set of customizations with their ISO file.",
+    "tags": [],
+    "title": "MicroWin",
+    "uri": "/user-guide/microwin/index.html"
+  },
+  {
+    "breadcrumb": "Welcome to the WinUtil Documentation!",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "User-Guides",
+    "uri": "/user-guide/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Welcome to the official documentation for WinUtil, your go-to utility for optimizing and managing your Windows environment. Whether you’re an IT professional, power user, or regular user, WinUtil provides a comprehensive set of tools to enhance your Windows experience.\nRunning the latest release of WinUtil You will first need to start a Powershell terminal as Admin. Now you can run the following command: irm christitus.com/win | iex\rInfo\rWinUtil is updated weekly as of the time of writing. Consequently, features and functionalities may evolve, and the documentation may not always reflect the most current images or information.",
+    "description": "Welcome to the official documentation for WinUtil, your go-to utility for optimizing and managing your Windows environment. Whether you’re an IT professional, power user, or regular user, WinUtil provides a comprehensive set of tools to enhance your Windows experience.",
+    "tags": [],
+    "title": "Welcome to the WinUtil Documentation!",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Welcome to the WinUtil Documentation!",
     "content": "",
     "description": "",
     "tags": [],
@@ -8,15 +32,7 @@ var relearn_searchindex = [
     "uri": "/categories/index.html"
   },
   {
-    "breadcrumb": "",
-    "content": "This is your new home page.",
-    "description": "This is your new home page.",
-    "tags": [],
-    "title": "Home Page",
-    "uri": "/index.html"
-  },
-  {
-    "breadcrumb": "Home Page",
+    "breadcrumb": "Welcome to the WinUtil Documentation!",
     "content": "",
     "description": "",
     "tags": [],
